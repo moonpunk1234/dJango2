efce49e7fcef88a16a4db0978bfc8bb45a3eb95b
moonpunk1234 <53620630+moonpunk1234@users.noreply.github.com>
2019-12-17 21:20:00 +0600

    Add files via upload

---
+++ Day02/Lesson2Proj/myApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def boot1(request):
+    return render(request,'myDesign1.html')
+
+
+def boot2(self):
+    return render(self,'myDesign2.html')
